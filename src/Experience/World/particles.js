b8cc3da2ffd67e3766774a6d90d45ad326306337
Ibrahim Hiarea <ibrahimhiarea@gmail.com>
2022-06-06 19:06:12 +0300

    Edit Particle

--- src/Experience/World/particles.js
+++ src/Experience/World/particles.js
@@ -8,18 +8,18 @@ class Particle {
         this.scene = this.experience.scene
 
         // particles number & position
-        this.count = 6000
+        this.count = 50000
         this.particlesGeometry = new THREE.BufferGeometry()
         this.positions = new Float32Array(this.count * 3)
         for (let i = 0; i < (this.count * 3); i++) {
-            this.positions[i] = ((Math.random() - 0.5) * 8000);
+            this.positions[i] = ((Math.random() - 0.5) * 50000);
             this.positions[i] <=0 ? this.positions[i]-=80 : this.positions[i]+=80;
         }
         this.particlesGeometry.setAttribute('position', new THREE.BufferAttribute(this.positions, 3))
         this.particlesMaterial = new THREE.PointsMaterial()
 
         // size & properties
-        this.particlesMaterial.size = 12
+        this.particlesMaterial.size = 30
         this.particlesMaterial.sizeAttenuation = true
         this.particlesMaterial.transparent = true
         this.particlesMaterial.alphaMap = textures.particle;
